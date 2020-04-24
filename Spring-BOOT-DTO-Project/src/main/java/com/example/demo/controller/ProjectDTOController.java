e4659380ec2a84540fce962cb138fd01a4fabb1c
libmjava11 <libmjava11@Java76.iiht.tech>
2020-04-24 18:18:46 +0530

    data rest commit

--- Spring-BOOT-DTO-Project/src/main/java/com/example/demo/controller/ProjectDTOController.java
+++ Spring-BOOT-DTO-Project/src/main/java/com/example/demo/controller/ProjectDTOController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +21,12 @@ public class ProjectDTOController
 {
 	private ProjectDTOService projectDTOService;
 	
+	@Autowired
+	public ProjectDTOController(ProjectDTOService projectDTOService) {
+		super();
+		this.projectDTOService = projectDTOService;
+	}
+
 	@GetMapping("/projects")
 	public ResponseEntity<List<ProjectDTO>> getAllProjects() 
 	{
